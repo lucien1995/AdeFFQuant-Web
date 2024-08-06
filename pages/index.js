74509369cedca42882b53636aca20d55053f652a
lucien1995 <1805198341@qq.com>
2024-08-07 06:33:04 +0930

    2

--- pages/index.js
+++ pages/index.js
@@ -12,37 +12,58 @@ import Services1 from "@/components/sections/Services1"
 import Slider1 from "@/components/sections/Slider1"
 import Testimonial1 from "@/components/sections/Testimonial1"
 import Whychoose1 from "@/components/sections/Whychoose1"
-export async function getServerSideProps(context) {
-  return {
-    redirect: {
-      destination: '/index-2',
-      permanent: false,
-    },
-  };
-}
-
-export default function Home() {
-  return null; // 这个组件可以返回null，因为它会被重定向
-}
-// export default function Home() {
-//
-//     return (
-//         <>
-//             <Layout headerStyle={1} footerStyle={1}>
-//                 <Slider1 />
-//                 <Services1 />
-//                 <About1 addClass={"pt-0"} />
-//                 <Marquee1 />
-//                 <Cta />
-//                 <Faqs />
-//                 <Whychoose1 />
-//                 <Testimonial1 />
-//                 <Projects1 />
-//                 <Funfact />
-//                 <News1 />
-//                 <Contact1 />
-//                 <Clients1 />
-//             </Layout>
-//         </>
-//     )
+import Slider2 from "@/components/sections/Slider2";
+import Services2 from "@/components/sections/Services2";
+import About2 from "@/components/sections/About2";
+import Whychoose2 from "@/components/sections/Whychoose2";
+import Team from "@/components/sections/Team";
+import Contact2 from "@/components/sections/Contact2";
+import Map from "@/components/sections/Map";
+// export async function getServerSideProps(context) {
+//   return {
+//     redirect: {
+//       destination: '/index-2',
+//       permanent: false,
+//     },
+//   };
 // }
+
+// export default function Home() {
+//   return null; // 这个组件可以返回null，因为它会被重定向
+// }
+export default function Home() {
+
+    return (
+        <>
+          <Layout headerStyle={2} footerStyle={1}>
+                <Slider2 />
+                <Services2 />
+                {/*<Marquee2 />*/}
+                <About2 />
+                {/*<Projects2 />*/}
+                <Whychoose2 />
+                {/*<Testimonial2 />*/}
+                <Team />
+                {/*<Clients2 />*/}
+                <Contact2 />
+                {/*<News2 />*/}
+                <Map />
+            </Layout>
+          {/*<Layout headerStyle={1} footerStyle={1}>*/}
+          {/*      <Slider1 />*/}
+          {/*      <Services1 />*/}
+          {/*      <About1 addClass={"pt-0"} />*/}
+          {/*      <Marquee1 />*/}
+          {/*      <Cta />*/}
+          {/*      <Faqs />*/}
+          {/*      <Whychoose1 />*/}
+          {/*      <Testimonial1 />*/}
+          {/*      <Projects1 />*/}
+          {/*      <Funfact />*/}
+          {/*      <News1 />*/}
+          {/*      <Contact1 />*/}
+          {/*      <Clients1 />*/}
+          {/*  </Layout>*/}
+        </>
+    )
+}
