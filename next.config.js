94bf39156dd5331788f80b3fb649f58f1ff7981e
Long <38904899+lucien1995@users.noreply.github.com>
2025-04-05 02:11:20 +1030

    update email & team members out.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isGithubPages = process.env.GITHUB_ACTIONS || false;
 
 export default {
   output: 'export',
+  trailingSlash: true,
   basePath: isGithubPages ? '/AdeFFQuant-Web' : '',
   images: {
     unoptimized: true,
