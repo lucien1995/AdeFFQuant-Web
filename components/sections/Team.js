d22ad01a6c5abde8eca1586498b6e2119a1d8ab0
Long <38904899+lucien1995@users.noreply.github.com>
2025-04-06 00:50:13 +1030

    update email & team members out.

--- components/sections/Team.js
+++ components/sections/Team.js
@@ -12,25 +12,25 @@ export default function Team() {
                         <h2>Meet the professional team <br />behind the success</h2>
                     </div>
                     <div className="row justify-content-center">
-//                        {/* Team block */}
-//                        <div className="team-block-two col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
-//                            <div className="inner-box">
-//                                <div className="info-box">
-//                                    <h4 className="name">Mr. FEI MAI</h4>
-//                                    <span className="designation">Director</span>
-//                                </div>
-//                                <div className="image-box">
-//                                    {/*<figure className="image"><Link href="/#"><img src="/images/resource/team-2.jpg" alt="" /></Link></figure>*/}
-//                                    {/*<div className="social-links">*/}
-//                                    {/*    <Link href="/index-2"><i className="fab fa-twitter" /></Link>*/}
-//                                    {/*    <Link href="/index-2"><i className="fab fa-facebook-f" /></Link>*/}
-//                                    {/*    <Link href="/index-2"><i className="fab fa-pinterest-p" /></Link>*/}
-//                                    {/*    <Link href="/index-2"><i className="fab fa-instagram" /></Link>*/}
-//                                    {/*</div>*/}
-//                                    {/*<span className="share-icon fa fa-plus" />*/}
-//                                </div>
-//                            </div>
-//                        </div>
+                        {/* Team block
+                        <div className="team-block-two col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
+                            <div className="inner-box">
+                                <div className="info-box">
+                                    <h4 className="name">Mr. FEI MAI</h4>
+                                    <span className="designation">Director</span>
+                                </div>
+                                <div className="image-box">
+                                    {/*<figure className="image"><Link href="/#"><img src="/images/resource/team-2.jpg" alt="" /></Link></figure>*/}
+                                    {/*<div className="social-links">*/}
+                                    {/*    <Link href="/index-2"><i className="fab fa-twitter" /></Link>*/}
+                                    {/*    <Link href="/index-2"><i className="fab fa-facebook-f" /></Link>*/}
+                                    {/*    <Link href="/index-2"><i className="fab fa-pinterest-p" /></Link>*/}
+                                    {/*    <Link href="/index-2"><i className="fab fa-instagram" /></Link>*/}
+                                    {/*</div>*/}
+                                    {/*<span className="share-icon fa fa-plus" />
+                                </div>
+                            </div>
+                        </div>*/}
                         {/* Team block */}
                         <div className="team-block-two col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="400ms">
                             <div className="inner-box">
--- components/sections/inner/TeamInner.js
+++ components/sections/inner/TeamInner.js
@@ -8,25 +8,25 @@ export default function TeamInner() {
             <section className="team-section">
                 <div className="auto-container">
                     <div className="row justify-content-center">
-//                        {/* Team block */}
-//                        <div className="team-block-two col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
-//                            <div className="inner-box">
-//                                <div className="info-box">
-//                                    <h4 className="name">Mr. FEI MAI</h4>
-//                                    <span className="designation">Director</span>
-//                                </div>
-//                                {/*<div className="image-box">*/}
-//                                {/*    /!*<figure className="image"><Link href="/#"><img src="/images/resource/team-1.jpg" alt="" /></Link></figure>*!/*/}
-//                                {/*    /!*<div className="social-links">*!/*/}
-//                                {/*    /!*    <Link href="/index-2"><i className="fab fa-twitter" /></Link>*!/*/}
-//                                {/*    /!*    <Link href="/index-2"><i className="fab fa-facebook-f" /></Link>*!/*/}
-//                                {/*    /!*    <Link href="/index-2"><i className="fab fa-pinterest-p" /></Link>*!/*/}
-//                                {/*    /!*    <Link href="/index-2"><i className="fab fa-instagram" /></Link>*!/*/}
-//                                {/*    /!*</div>*!/*/}
-//                                {/*    /!*<span className="share-icon fa fa-plus" />*!/*/}
-//                                {/*</div>*/}
-//                            </div>
-//                        </div>
+                        {/* Team block
+                        <div className="team-block-two col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
+                            <div className="inner-box">
+                                <div className="info-box">
+                                    <h4 className="name">Mr. FEI MAI</h4>
+                                    <span className="designation">Director</span>
+                                </div>
+                                {/*<div className="image-box">*/}
+                                {/*    /!*<figure className="image"><Link href="/#"><img src="/images/resource/team-1.jpg" alt="" /></Link></figure>*!/*/}
+                                {/*    /!*<div className="social-links">*!/*/}
+                                {/*    /!*    <Link href="/index-2"><i className="fab fa-twitter" /></Link>*!/*/}
+                                {/*    /!*    <Link href="/index-2"><i className="fab fa-facebook-f" /></Link>*!/*/}
+                                {/*    /!*    <Link href="/index-2"><i className="fab fa-pinterest-p" /></Link>*!/*/}
+                                {/*    /!*    <Link href="/index-2"><i className="fab fa-instagram" /></Link>*!/*/}
+                                {/*    /!*</div>*!/*/}
+                                {/*    /!*<span className="share-icon fa fa-plus" />*!/*/}
+                                {/*</div>
+                            </div>
+                        </div>*/}
                         {/*/!* Team block *!/*/}
                         <div className="team-block-two col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="400ms">
                             <div className="inner-box">
@@ -46,25 +46,25 @@ export default function TeamInner() {
                                 {/*</div>*/}
                             </div>
                         </div>
-//                        {/* Team block */}
-//                        <div className="team-block-two col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="800ms">
-//                            <div className="inner-box">
-//                                <div className="info-box">
-//                                    <h4 className="name">Mrs. DUODUO LI</h4>
-//                                    <span className="designation">CMO</span>
-//                                </div>
-//                                {/*<div className="image-box">*/}
-//                                {/*    <figure className="image"><Link href="/#"><img src="/images/resource/team-3.jpg" alt="" /></Link></figure>*/}
-//                                {/*    <div className="social-links">*/}
-//                                {/*        <Link href="/#"><i className="fab fa-twitter" /></Link>*/}
-//                                {/*        <Link href="/#"><i className="fab fa-facebook-f" /></Link>*/}
-//                                {/*        <Link href="/#"><i className="fab fa-pinterest-p" /></Link>*/}
-//                                {/*        <Link href="/#"><i className="fab fa-instagram" /></Link>*/}
-//                                {/*    </div>*/}
-//                                {/*    <span className="share-icon fa fa-plus" />*/}
-//                                {/*</div>*/}
-//                            </div>
-//                        </div>
+                        {/* Team block
+                        <div className="team-block-two col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="800ms">
+                            <div className="inner-box">
+                                <div className="info-box">
+                                    <h4 className="name">Mrs. DUODUO LI</h4>
+                                    <span className="designation">CMO</span>
+                                </div>
+                                {/*<div className="image-box">*/}
+                                {/*    <figure className="image"><Link href="/#"><img src="/images/resource/team-3.jpg" alt="" /></Link></figure>*/}
+                                {/*    <div className="social-links">*/}
+                                {/*        <Link href="/#"><i className="fab fa-twitter" /></Link>*/}
+                                {/*        <Link href="/#"><i className="fab fa-facebook-f" /></Link>*/}
+                                {/*        <Link href="/#"><i className="fab fa-pinterest-p" /></Link>*/}
+                                {/*        <Link href="/#"><i className="fab fa-instagram" /></Link>*/}
+                                {/*    </div>*/}
+                                {/*    <span className="share-icon fa fa-plus" />*/}
+                                {/*</div>
+                            </div>
+                        </div> */}
 
                     </div>
                 </div>
